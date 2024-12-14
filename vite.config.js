1ef840c87f710ba7489a994562f475df8f170ae6
mtysa <150185875+mtysa@users.noreply.github.com>
2024-12-13 22:00:06 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/final-dinner-frontend",
+  base: "/final-dinner-frontend/",
   plugins: [react()],
   server: {
     port: 3005,
